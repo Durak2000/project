16f54c551acf6305621ca4c22a2c32f438eddfb4
Durak <fetisovtimur0@gmail.com>
2024-11-30 17:51:21 +0300

    Немного изменён код бота

--- main.py
+++ main.py
@@ -1,23 +1,42 @@
 import asyncio
 import logging
+import sys
+from os import getenv
 
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot, Dispatcher, html
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
+from aiogram.types import Message
 
-TOKEN = '7984849829:AAF9Ru8YrSKepje_8McB_a9x6HQmZ6yNvTA'
-bot = Bot(TOKEN)
+# Bot token can be obtained via https://t.me/BotFather
+TOKEN = "7984849829:AAF9Ru8YrSKepje_8McB_a9x6HQmZ6yNvTA"
+
+# All handlers should be attached to the Router (or Dispatcher)
+
 dp = Dispatcher()
 
+
 @dp.message(CommandStart())
-async def start_cmd(message: types.Message):
-    await message.answer('Привет, я Дон пидиди! Я буду проводить для тебя игры в мафию')
+async def command_start_handler(message: Message) -> None:
+    await message.answer(f"Привет, {html.bold(message.from_user.full_name)}, я Дон местной мафии")
+    await message.answer(f'Хочешь начать игру в мафию?')
+
 
 @dp.message()
-async def echo(message: types.Message):
-    await message.answer(f'Я пока что в разработке так что могу толко показать вам ваше же предложение: {message.text}')
+async def echo_handler(message: Message) -> None:
+    try:
+        await message.send_copy(chat_id=message.chat.id)
+    except TypeError:
+        await message.answer("Nice try!")
 
-async def main():
-    logging.basicConfig(level=logging.INFO)
+
+async def main() -> None:
+    bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+
     await dp.start_polling(bot)
 
-asyncio.run(main())
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(main())
