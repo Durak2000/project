e72afdaa592a6cf6089a936c1e1f67fd30349713
Durak <fetisovtimur0@gmail.com>
2025-01-14 10:56:12 +0300

    Сделаны reply

--- handlers/user_private.py
+++ handlers/user_private.py
@@ -1,11 +1,14 @@
 from aiogram import F,  html, types, Router
 from aiogram.filters import CommandStart, Command, or_f
+from aiogram.utils.formatting import as_list, as_marked_section, Bold
+
 from filters.chat_types import ChatTypeFilter
 
 from kbdn import reply
 
 user_private_router = Router()
 user_private_router.message.filter(ChatTypeFilter(['private']))
+
 
 @user_private_router.message(CommandStart())
 async def start_cmd(message: types.Message):
@@ -15,16 +18,20 @@ async def start_cmd(message: types.Message):
                              resize_keyboard=True,
                              input_feild_placeholder='Что ты хочешь?'))
 
+
 @user_private_router.message(or_f(Command("menu"), (F.text.lower() == "меню")))
 async def menu_cmd(message: types.Message):
     await message.answer('Меню: ', reply_markup=reply.del_kbd)
+
 
 @user_private_router.message(or_f(Command("about"), (F.text.lower() == "о нас")))
 async def menu_cmd(message: types.Message):
     await message.answer('Привет я разработчик этого прекрасного бота\
  (https://t.me/Kitty2_2_8), этот бот сделан для того чтобы проводить игры в\
- мафию для всей вашей весёлой компании и впринципе чтобы хорошо провести время,\
- если будут какие нибудь вопросы или предложения по функционалу бота то можете написать мне в личку, приятной игры :)')
+ мафию для всей вашей весёлой компании и впринципе чтобы хорошо\
+ провести время, если будут какие нибудь вопросы или предложения\
+ по функционалу бота то можете написать мне в личку, приятной игры :)')
+
 
 @user_private_router.message(F.text.lower().contains('52'))
 async def menu_cmd(message: types.Message):
@@ -34,6 +41,19 @@ async def menu_cmd(message: types.Message):
  Альбом, он чисто мой, никому его не продам (он мой)\
  Не думаю о том, как хорошо было вчера')
 
+
 @user_private_router.message(F.text)
 async def menu_cmd(message: types.Message):
     await message.answer(message.text)
+
+
+@user_private_router.message(F.contact)
+async def get_contact(message: types.Message):
+    await message.answer(f"номер получен")
+    await message.answer(str(message.contact))
+
+
+@user_private_router.message(F.location)
+async def get_location(message: types.Message):
+    await message.answer(f"локация получена")
+    await message.answer(str(message.location))
--- kbdn/reply.py
+++ kbdn/reply.py
@@ -5,7 +5,9 @@ start_kb = ReplyKeyboardBuilder()
 start_kb.add(
     KeyboardButton(text='Меню'),
     KeyboardButton(text='О нас'),
+    KeyboardButton(text="Отправить номер ☎️", request_contact=True),
+    KeyboardButton(text="Отправить локацию 🗺️", request_location=True),
 )
-start_kb.adjust(1, 1)
+start_kb.adjust(2, 2)
 
 del_kbd = ReplyKeyboardRemove()
